a9418cbc27e3fc16fec8796af3fcf8024c109498
emmajphillips <emmjphillips@gmail.com>
2020-06-22 12:19:45 +0100

    updated sections to be roughly 100vh, created inital event listeners on hero div

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,19 @@
 function init() {
-  console.log('It works!')
+  const moveableText = document.querySelector('h1')
+  const moveableTextContainer = document.querySelector('.hero')
+  console.log(moveableText)
+  console.log(moveableTextContainer)
+
+  function followMouse() {
+    console.log('mouse entered')
+  }
+
+  function stopFollowingMouse() {
+    console.log('mouse left')
+  }
+
+  moveableTextContainer.addEventListener('mouseenter', followMouse)
+  moveableTextContainer.addEventListener('mouseleave', stopFollowingMouse)
 
 }
 
