6c3cdc536df4662b2c1331d11cc78d6bbbe34941
emmajphillips <emmjphillips@gmail.com>
2020-06-23 10:59:14 +0100

    inital implementation of text following mouse within hero section

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,23 @@
 function init() {
-  const moveableText = document.querySelector('h1')
+  const moveableText = document.querySelector('#moveable-text')
   const moveableTextContainer = document.querySelector('.hero')
-  console.log(moveableText)
-  console.log(moveableTextContainer)
 
-  function followMouse() {
-    console.log('mouse entered')
+  
+  function followMouse(event) {
+    event.preventDefault()
+    const letters = moveableText.textContent.replace(/\s/g, '').split('').join('')
+    moveableText.textContent = letters
+    document.body.appendChild(moveableText)
+    moveableText.style.position = 'absolute'
+    moveableText.style.left = event.clientX + 'px'
+    moveableText.style.top = event.clientY + 'px'
   }
 
   function stopFollowingMouse() {
     console.log('mouse left')
   }
 
-  moveableTextContainer.addEventListener('mouseenter', followMouse)
+  moveableTextContainer.addEventListener('mousemove', followMouse)
   moveableTextContainer.addEventListener('mouseleave', stopFollowingMouse)
 
 }
