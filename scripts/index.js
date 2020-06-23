1a2966795aa7a6e1f0e40c8e7900e7d9de2c0c91
emmajphillips <emmjphillips@gmail.com>
2020-06-23 17:40:40 +0100

    removed javascipt around hero section

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,6 @@
 function init() {
-  const moveableText = document.querySelector('#moveable-text')
-  const moveableTextContainer = document.querySelector('.hero')
+console.log('it works')
 
-  
-  function followMouse(event) {
-    event.preventDefault()
-    const letters = moveableText.textContent.replace(/\s/g, '').split('').join('')
-    moveableText.textContent = letters
-    document.body.appendChild(moveableText)
-    moveableText.style.position = 'absolute'
-    moveableText.style.left = event.clientX + 'px'
-    moveableText.style.top = event.clientY + 'px'
-  }
 
-  function stopFollowingMouse() {
-    console.log('mouse left')
-  }
-
-  moveableTextContainer.addEventListener('mousemove', followMouse)
-  moveableTextContainer.addEventListener('mouseleave', stopFollowingMouse)
-
-}
 
 window.addEventListener('DOMContentLoaded', init)
